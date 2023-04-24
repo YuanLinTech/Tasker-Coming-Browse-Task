40a1c060784d2009dde8a005d6d7497bcf9c33cd
Yuan Lin <yu.an.lintech@gmail.com>
2023-04-24 10:33:20 +1000

    cleaned up folder

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { Provider } from 'react-redux';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './features/store';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import GlobalStyle from './Theme/globalStyles';
-import theme from './Theme/Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
--- src/pages/Login.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoginModal from '../components/UserLoginPage/LoginModal';
-
-function Login() {
-  return <LoginModal />;
-}
-
-export default Login;
